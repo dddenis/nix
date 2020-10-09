56b836d3f791086b32f6a605db8df4413fc79df1
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2020-10-09 18:35:18 +0200

    feat(services): add lorri bin

--- services/lorri/default.nix
+++ services/lorri/default.nix
@@ -1,9 +1,13 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let cfg = config.services.lorri;
 
 in {
   options.services.lorri.enable' = lib.mkEnableOption "lorri";
 
-  config = lib.mkIf cfg.enable' { programs.direnv.enable' = true; };
+  config = lib.mkIf cfg.enable' {
+    home.packages = [ pkgs.lorri ];
+
+    programs.direnv.enable' = true;
+  };
 }
