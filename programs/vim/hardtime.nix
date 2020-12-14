749db35cab6d0919bc93ee8baa05c0b8f2ffefc6
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2020-12-14 03:43:10 +0100

    feat(vim): ignore hardtime quickfix

--- programs/vim/hardtime.nix
+++ programs/vim/hardtime.nix
@@ -9,6 +9,7 @@ in {
     programs.neovim = {
       extraConfig = ''
         let g:hardtime_default_on = 1
+        let g:hardtime_ignore_quickfix = 1
       '';
 
       plugins = with pkgs.vimPlugins; [ vim-hardtime ];
