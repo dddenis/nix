d4f4bcd179af8c65af4d028f881a3371882a27da
Denis Goncharenko <dddenjer@gmail.com>
2020-12-27 02:55:02 +0100

    feat(unityhub): add v2.4.2 overlay

--- programs/unityhub/overlay.nix
+++ programs/unityhub/overlay.nix
@@ -2,4 +2,13 @@ self: super:
 
 {
   unity-androidenv = super.callPackage ./androidenv.nix { };
+
+  # 2.4.2
+  unityhub = super.unityhub.override {
+    fetchurl = _:
+      super.fetchurl {
+        url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage";
+        sha256 = "mtx2mK36ZHn4NysLo157HX4q4l89d84jhmyws832gVQ=";
+      };
+  };
 }
