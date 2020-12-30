945fc3dd3de4649303e395194421c52b66981044
Denis Goncharenko <dddenjer@gmail.com>
2020-12-30 04:25:43 +0100

    feat(unityhub): add desktop item

--- programs/unityhub/module-home.nix
+++ programs/unityhub/module-home.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.programs.unityhub;
+let
+  cfg = config.programs.unityhub;
+
+  desktopItem = pkgs.makeDesktopItem {
+    name = "UnityHub";
+    desktopName = "UnityHub";
+    exec = "${pkgs.unityhub}/bin/unityhub";
+  };
 
 in {
   options.programs.unityhub.enable' = lib.mkEnableOption "unityhub";
 
-  config = lib.mkIf cfg.enable' { home.packages = [ pkgs.unityhub ]; };
+  config =
+    lib.mkIf cfg.enable' { home.packages = [ desktopItem pkgs.unityhub ]; };
 }
