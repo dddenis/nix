13bc9dca043868a40e520318d960c7e948c8aa1d
Denis Goncharenko <dddenjer@gmail.com>
2020-12-30 01:39:11 +0100

    feat(ddd-pc): update bookmarks

--- hosts/ddd-pc/configuration.nix
+++ hosts/ddd-pc/configuration.nix
@@ -1,11 +1,16 @@
 { config, lib, pkgs, ... }:
 
-{
+let bookmarks = { vn = "/dev/dddenis/vn"; };
+
+in {
   imports = [ ./hardware-configuration.nix ];
 
   boot.blacklistedKernelModules = [ "sp5100_tco" ];
 
-  time.timeZone = "Europe/Berlin";
+  fonts.fontconfig.defaultFonts = {
+    monospace =
+      lib.mkBefore [ "Iosevka DDD Extended" "Iosevka Nerd Font Mono" ];
+  };
 
   hardware = {
     bluetooth.enable = true;
@@ -18,10 +23,9 @@
     };
   };
 
-  fonts.fontconfig.defaultFonts = {
-    monospace =
-      lib.mkBefore [ "Iosevka DDD Extended" "Iosevka Nerd Font Mono" ];
-  };
+  home-manager.users.ddd.home.bookmarks = builtins.mapAttrs
+    (_: path: config.home-manager.users.ddd.home.homeDirectory + path)
+    bookmarks;
 
   services = {
     safeeyes.enable = true;
@@ -40,4 +44,6 @@
 
   systemd.services.bluetooth.serviceConfig.ExecStart =
     [ "" "${pkgs.bluez}/bin/bluetoothd --noplugin=sap" ];
+
+  time.timeZone = "Europe/Berlin";
 }
