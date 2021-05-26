cdd2029c0fe3d2ddaf5eab6f2278ad16ae3e489b
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2021-05-26 10:10:21 +0200

    feat(tmux): add choose-tree keybinding

--- programs/tmux/module-home.nix
+++ programs/tmux/module-home.nix
@@ -33,6 +33,8 @@ in {
       '';
 
       extraConfig = lib.mkBefore ''
+        bind q choose-tree -s -Z
+
         bind s split-window -v -c '#{pane_current_path}'
         bind v split-window -h -c '#{pane_current_path}'
 
