7f59a9b1ff981664400b00b1069cfad4544ed72f
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2021-05-26 19:36:11 +0200

    feat(lazygit): add log commands

--- programs/lazygit/module-home.nix
+++ programs/lazygit/module-home.nix
@@ -2,8 +2,12 @@
 
 let
   cfg = config.programs.lazygit;
+
   delta = "${pkgs.delta}/bin/delta";
   lazygit = "${pkgs.lazygit}/bin/lazygit";
+
+  logCmd =
+    "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'";
 
 in {
   options.programs.lazygit.enable' = lib.mkEnableOption "lazygit";
@@ -14,9 +18,14 @@ in {
         enable = true;
 
         settings = {
-          git.paging = {
-            colorArg = "always";
-            pager = "${delta} --paging=never";
+          git = {
+            branchLogCmd = "${logCmd} {{branchName}} --";
+            allBranchesLogCmd = "${logCmd} --all";
+
+            paging = {
+              colorArg = "always";
+              pager = "${delta} --paging=never";
+            };
           };
 
           gui.showCommandLog = false;
