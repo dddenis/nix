a5e8059f5e8397b7be101a320b7b322f745a18e4
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2021-06-09 11:55:28 +0200

    fix(tmux): use darwing specific terminfo

--- programs/tmux/module-nixos-darwin.nix
+++ programs/tmux/module-nixos-darwin.nix
@@ -4,19 +4,14 @@ let
   isEnabled =
     lib.user.anyConfig (userConfig: userConfig.programs.tmux.enable') config;
 
-  terminfo = pkgs.stdenv.mkDerivation {
-    pname = "terminfo";
-    inherit (pkgs.ncurses) src version;
-
-    phases = [ "unpackPhase" "installPhase" ];
-
-    installPhase = ''
-      cp misc/terminfo.src $out
-    '';
+  tmux-256color = builtins.fetchurl {
+    url =
+      "https://gist.githubusercontent.com/nicm/ea9cf3c93f22e0246ec858122d9abea1/raw/37ae29fc86e88b48dbc8a674478ad3e7a009f357/tmux-256color";
+    sha256 = "18znnmsl53nhv0gxacvd6435599afsz2ig0wyylbj04rchqrl9cg";
   };
 
 in lib.mkIf isEnabled {
   environment.extraInit = ''
-    tic -xe tmux,tmux-256color ${terminfo}
+    tic -xe tmux-256color ${tmux-256color}
   '';
 }
