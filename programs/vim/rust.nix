5be33faa0a0e8b679761216dc322a774a0a632d5
Denis Goncharenko <denis.goncharenko@mcmakler.de>
2021-07-08 22:38:57 +0200

    feat(vim): add clippy to rust

--- programs/vim/rust.nix
+++ programs/vim/rust.nix
@@ -6,12 +6,13 @@ in {
   options.programs.vim.rust.enable' = lib.mkEnableOption "vim.rust";
 
   config = lib.mkIf cfg.enable' (lib.mkMerge [
-    { programs.neovim.extraPackages = with pkgs; [ rustfmt ]; }
+    { programs.neovim.extraPackages = with pkgs; [ clippy rustfmt ]; }
 
     (lib.mkIf config.programs.vim.coc-nvim.enable' {
       programs.neovim.plugins = with pkgs.vimPlugins; [ coc-rust-analyzer ];
 
       programs.vim.coc-nvim.coc-settings = {
+        "rust-analyzer.checkOnSave.command" = "clippy";
         "rust-analyzer.serverPath" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
       };
     })
