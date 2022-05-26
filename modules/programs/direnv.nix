5ddc9b6f2da445001170dec66ef16ad20ad86559
Denis Goncharenko <denis.goncharenko@kontist.com>
2022-05-27 01:37:08 +0200

    feat(direnv): add export_alias

--- modules/programs/direnv.nix
+++ modules/programs/direnv.nix
@@ -14,6 +14,23 @@ in {
     hm.programs.direnv = {
       enable = true;
       enableZshIntegration = true;
+
+      stdlib = ''
+        export_alias() {
+          local name=$1
+          shift
+          local alias_dir=$PWD/.direnv/aliases
+          local target="$alias_dir/$name"
+          mkdir -p "$alias_dir"
+          if ! [[ ":$PATH:" == *":$alias_dir:"* ]]; then
+            PATH_add "$alias_dir"
+          fi
+
+          echo "#!/usr/bin/env -S bash -e" > "$target"
+          echo "$@" >> "$target"
+          chmod +x "$target"
+        }
+      '';
     };
   };
 }
