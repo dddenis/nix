fc68a152679a954e8ab6803664f60debcf416883
Denis Goncharenko <dddenjer@gmail.com>
2022-07-06 21:37:04 +0200

    fix(templates): update devshell

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -1,16 +1,21 @@
 {
   inputs = {
     devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.inputs.flake-utils.follows = "flake-utils";
+
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, devshell, flake-utils, nixpkgs }:
-    flake-utils.lib.simpleFlake {
+    let overlay = _: prev: { };
+
+    in flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
 
       name = "devshell";
-      preOverlays = [ devshell.overlay ];
+      preOverlays = [ devshell.overlay overlay ];
       systems = flake-utils.lib.defaultSystems;
 
       shell = { pkgs }:
