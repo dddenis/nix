b776ae4b649da18ee05fb2cb0a8e118666a3ae6a
Denis Goncharenko <denis.goncharenko@kontist.com>
2022-08-25 20:21:40 +0200

    feat(nix): add nix-community cache

--- modules/profiles/shared.nix
+++ modules/profiles/shared.nix
@@ -4,6 +4,8 @@ let
   caches = {
     "https://hydra.iohk.io" =
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+    "https://nix-community.cachix.org" =
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
   };
 
 in {
