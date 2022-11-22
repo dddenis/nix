4a95926c5d43cae5406de9a14a29114e9e475dc3
Denis Goncharenko <denis.goncharenko@kontist.com>
2022-11-22 10:28:38 +0100

    feat(neovim): add shell formatting

--- flakes/neovim/modules/shell.nix
+++ flakes/neovim/modules/shell.nix
@@ -3,8 +3,15 @@
 {
   config.programs.ddd.neovim.lsp.client.bashls = { };
 
+  config.programs.ddd.neovim.lsp.null-ls.sources = [
+    "require'null-ls'.builtins.formatting.shellharden"
+    "require'null-ls'.builtins.formatting.shfmt"
+  ];
+
   config.programs.ddd.neovim.packages = with pkgs; [
     nodePackages.bash-language-server
     shellcheck
+    shellharden
+    shfmt
   ];
 }
