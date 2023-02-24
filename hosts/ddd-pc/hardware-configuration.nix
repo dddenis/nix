41c9cac11be4595b437c9f9922a55200b18ae7b3
Denis Goncharenko <dddenjer@gmail.com>
2023-02-25 00:24:10 +0100

    fix(ddd-pc): update hardware configuration

--- hosts/ddd-pc/hardware-configuration.nix
+++ hosts/ddd-pc/hardware-configuration.nix
@@ -27,5 +27,14 @@
     [ { device = "/dev/disk/by-uuid/88ffb7dd-0811-4e50-ac31-82858b2f3aea"; }
     ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp34s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
