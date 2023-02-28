54a1fb648b0051ec36fda12f35b8488c334fc1c1
Denis Goncharenko <dddenjer@gmail.com>
2023-02-28 23:17:56 +0100

    feat(ddd-complyance): add company git condition include

--- hosts/ddd-complyance/default.nix
+++ hosts/ddd-complyance/default.nix
@@ -77,6 +77,19 @@
     ];
   };
 
+  home-manager.users.ddd.programs = {
+    git = {
+      includes = [
+        {
+          condition = "hasconfig:remote.*.url:git@github.com:complyance/**";
+          contents = {
+            user.email = "denis.goncharenko@complyance.com";
+          };
+        }
+      ];
+    };
+  };
+
   home-manager.users.ddd.ddd.programs = {
     alacritty.enable = true;
     atool.enable = true;
