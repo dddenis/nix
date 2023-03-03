ee81755eb0c4f76bf808dc34acf4224caaee0433
Denis Goncharenko <dddenjer@gmail.com>
2023-03-04 00:16:09 +0100

    fix(safeeyes): update config files

--- modules/home-manager/services/safeeyes/default.nix
+++ modules/home-manager/services/safeeyes/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, options, ... }:
+{ config, lib, pkgs, ... }:
 
 let cfg = config.ddd.services.safeeyes;
 
@@ -8,6 +8,16 @@ in
 
   config = lib.mkIf cfg.enable {
     services.safeeyes.enable = true;
-    xdg.configFile."safeeyes/safeeyes.json".source = ./safeeyes.json;
+    services.safeeyes.package = pkgs.unstable.safeeyes;
+
+    systemd.user.services.safeeyes.Service.Environment = [
+      # Fix overlay covering only one monitor on Wayland
+      "GDK_BACKEND=x11"
+    ];
+
+    xdg.configFile."safeeyes/safeeyes.json".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.configPath}/modules/home-manager/services/safeeyes/safeeyes.json";
+    xdg.configFile."safeeyes/style/safeeyes_style.css".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.configPath}/modules/home-manager/services/safeeyes/safeeyes_style.css";
   };
 }
