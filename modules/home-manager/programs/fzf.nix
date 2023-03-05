bbaf3239df15f3d89f9966530a8ffe3561b9b6d1
Denis Goncharenko <dddenjer@gmail.com>
2023-03-05 22:09:27 +0100

    fix(fzf): append history search instead of replace

--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -28,6 +28,24 @@ in
       _fzf_compgen_dir() {
         ${fd} --type d --hidden --follow --exclude .git . "$1"
       }
+
+      fzf-history-widget () {
+          local selected num
+          setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+          selected=($(fc -rl 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
+          FZF_DEFAULT_OPTS="--height ''${FZF_TMUX_HEIGHT:-40%} ''${FZF_DEFAULT_OPTS-} -n2..,.. --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore ''${FZF_CTRL_R_OPTS-} +m" $(__fzfcmd))) 
+          local ret=$? 
+          if [ -n "$selected" ]
+          then
+              num=$selected[1] 
+              if [ -n "$num" ]
+              then
+                  LBUFFER="$LBUFFER$(fc -ln "$num" "$num")"
+              fi
+          fi
+          zle reset-prompt
+          return $ret
+      }
     '';
   };
 }
