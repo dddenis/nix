568797258eff26248b6ed273b6b5a8e01af0e36a
Denis Goncharenko <dddenjer@gmail.com>
2023-03-30 23:31:51 +0200

    fix(neovim): update flake inputs

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     kmonad.inputs.nixpkgs.follows = "nixos";
 
     neovim.url = "path:./flakes/neovim";
-    neovim.inputs.nixpkgs.follows = "nixos";
+    neovim.inputs.nixpkgs.follows = "nixpkgs";
     neovim.inputs.flake-utils.follows = "flake-utils";
 
     ipu6.url = "path:./flakes/ipu6";
