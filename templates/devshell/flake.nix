596896d58435984ff44cddf0c2a612bb459c9ffa
Denis Goncharenko <dddenjer@gmail.com>
2023-04-02 22:26:03 +0200

    feat(templates): update devshell

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -3,20 +3,22 @@
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
     devshell.inputs.flake-utils.follows = "flake-utils";
-
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, devshell, flake-utils, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     let overlay = _: prev: { };
 
-    in flake-utils.lib.simpleFlake {
+    in
+    flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
 
       name = "devshell";
-      preOverlays = [ devshell.overlay overlay ];
       systems = flake-utils.lib.defaultSystems;
+
+      preOverlays = [
+        inputs.devshell.overlays.default
+        overlay
+      ];
 
       shell = { pkgs }:
         pkgs.devshell.mkShell {
