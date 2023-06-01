a4c21b60f4862ff41a11122af9d17e350dc936da
Denis Goncharenko <dddenjer@gmail.com>
2023-06-01 14:38:43 +0200

    fix(neovim): remove tab keymap overrides

--- modules/home-manager/programs/neovim/nvim/lua/config/keymaps.lua
+++ modules/home-manager/programs/neovim/nvim/lua/config/keymaps.lua
@@ -3,11 +3,6 @@ Map("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "Clear Search" })
 Map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 Map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
-Map("n", "gt", "<nop>")
-Map("n", "<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-Map("n", "gT", "<nop>")
-Map("n", "<s-tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
-
 Map("n", "<s-up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 Map("n", "<s-down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 Map("n", "<s-left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
