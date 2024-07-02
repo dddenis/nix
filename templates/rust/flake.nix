e3a1b41ccf5bd3b213303b978ade771da3cd5b49
Denis Goncharenko <dddenjer@gmail.com>
2024-07-02 22:44:36 +0200

    fix(templates): update rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,50 +1,58 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devshell.inputs.flake-utils.follows = "flake-utils";
 
     fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = _: prev: { };
+  outputs = { nixpkgs, fenix, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 
-        pkgs = import nixpkgs {
-          inherit system;
+      overlay = _: prev: { };
 
-          overlays = [
-            inputs.devshell.overlays.default
-            inputs.fenix.overlays.default
-            overlay
+      perSystem = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              fenix.overlays.default
+              overlay
+            ];
+          };
+
+        in
+        f pkgs
+      );
+
+    in
+    {
+      devShells = perSystem (pkgs:
+        let
+          rust-toolchain = pkgs.fenix.complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
           ];
-        };
 
-        toolchain = pkgs.fenix.complete.withComponents [
-          "cargo"
-          "clippy"
-          "rust-src"
-          "rustc"
-          "rustfmt"
-        ];
-
-      in
-      {
-        devShell = pkgs.devshell.mkShell {
-          motd = "";
-
-          packages = with pkgs; [
-            stdenv.cc
-            toolchain
-            fenix.rust-analyzer
-          ];
-        };
-      }
-    );
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              libiconv
+              rust-toolchain
+              rust-analyzer-nightly
+            ];
+          };
+        });
+    };
 }
+
