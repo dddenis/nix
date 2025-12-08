e5e8987279759132eeaaa999bce0b7d6a7c32728
Denis Goncharenko <dddenjer@gmail.com>
2025-12-08 15:59:07 +0100

    feat(home): add abra build machine

--- modules/home-manager/hosts/abra.nix
+++ modules/home-manager/hosts/abra.nix
@@ -7,6 +7,17 @@ in
   options.ddd.hosts.abra.enable = lib.mkEnableOption "abra";
 
   config = lib.mkIf cfg.enable {
+    nix.buildMachines = [
+      {
+        hostName = "abra";
+        systems = [ "aarch64-linux" ];
+        protocol = "ssh-ng";
+        sshUser = "builder";
+        sshKey = config.sops.secrets."ssh/keys/builder@ddd-complyance".path;
+      }
+    ];
+    sops.secrets."ssh/keys/builder@ddd-complyance" = { };
+
     programs.ssh.includes = [
       config.sops.secrets."ssh/includes/abra".path
     ];
