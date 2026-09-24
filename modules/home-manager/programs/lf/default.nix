86e590089bcc6ab308038d8de5879b12818bd1f1
Denis Goncharenko <dddenjer@gmail.com>
2026-09-24 21:52:49 +0200

    feat(lf): follow current directory when quitting with Q

--- modules/home-manager/programs/lf/default.nix
+++ modules/home-manager/programs/lf/default.nix
@@ -12,5 +12,21 @@ in
 
     xdg.configFile."lf/lfrc".source = config.lib.file.mkOutOfStoreSymlink
       "${config.home.configPath}/modules/home-manager/programs/lf/lfrc";
+
+    programs.zsh.initContent = ''
+      lf() {
+        local dir_file dir ret=0
+        dir_file="$(mktemp "''${TMPDIR:-/tmp}/lf-cd.XXXXXX")" || return
+        {
+          LF_CD_FILE="$dir_file" command lf "$@" || ret=$?
+          if IFS= read -r -d "" dir < "$dir_file"; then
+            builtin cd -- "$dir" || ret=$?
+          fi
+        } always {
+          command rm -f -- "$dir_file"
+        }
+        return "$ret"
+      }
+    '';
   };
 }
